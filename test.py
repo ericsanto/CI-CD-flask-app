7041ecc3c46e7adba956460595af3eb8c7d678e7
eric <ericjesus403@gmail.com>
2024-09-26 11:02:22 -0300

    feat: Testa a aplicação flask do arquivo app.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import pytest
+from flask import Flask
+
+# Supondo que o seu app esteja em um arquivo chamado app.py
+from app import app  # Altere "app" para o nome do seu arquivo, se necessário
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.data == b'Hello. Iam FLask'
+    assert response.status_code == 200
+
